0be048c876580b0f87a68ec04ce9d9baff6c600c
Adriano Grimolizzi <a.grimolizzi@gmail.com>
2019-05-07 15:50:56 +0200

    Update globalScope.js

--- globalScope.js
+++ globalScope.js
@@ -1,8 +1,10 @@
-//a globally-scoped variable:
+// Scope is hierarchical, at the base of the tree is the scope that you're implicitly in when you start a program, the Global Scope.
+// When a JS program starts, before any function is called, it is executing in global scope.
+// Anything you declare in global scope will be available to all scopes of the program.
 
 var a = 1;
 
-function one() {
+function one() { // This function is dependant on global scope.
     console.log(a);
 }
 
