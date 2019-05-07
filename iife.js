da6141e09bffa6cf8b7fb0de4d6a7044d28ef3b1
Adriano Grimolizzi <a.grimolizzi@gmail.com>
2019-05-07 16:40:03 +0200

    Update iife.js

--- iife.js
+++ iife.js
@@ -1,7 +1,16 @@
-var sayHello = function() {
-    console.log("Hello!");
-}
+// Immediately Invoked function expression - an IIFE declares a function and then runs it immediately.
 
-sayHello();
+(function() {
+    console.log("qwerty");
+})();
 
-console.log(typeof(sayHello)); //function
+// Advantages: anything inside it has its own scope, and because it is a function, it can pass something out of the scope:
+
+// This function reports the number of thimes it's been called in a way that can't be tampered with:
+
+const f = (function() {
+    let count = 0;
+    return function() {
+        return `I have been called ${++count} time(s).`;
+    }
+})();
